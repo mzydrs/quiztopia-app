81e2629c0566c6b7c0fa6f24d787558b7ec5e00c
mdrszy <mdrszy@gmail.com>
2024-12-13 00:07:00 +0700

    first backend commit

---
+++ backend/src/main/java/com/quiztopia/QuiztopiaApplication.java
@@ -0,0 +1,13 @@
+package com.quiztopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiztopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiztopiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/quiztopia/QuiztopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiztopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiztopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
